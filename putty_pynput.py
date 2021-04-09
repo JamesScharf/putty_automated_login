88490f1c21f9f5693b4052392d0f28e360d2c9fc
J <10171725+JamesScharf@users.noreply.github.com>
2021-04-08 21:33:32 -0400

    Update putty_pynput.py

--- putty_pynput.py
+++ putty_pynput.py
@@ -5,36 +5,56 @@ from time import sleep
 def type_word(word):
     for char in word:
         keyboard.press(char)
+        keyboard.release(char)
 
     keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 
 
 while True:
     # open up putty
     keyboard = Controller()
     keyboard.press(Key.cmd)
+    keyboard.release(Key.cmd)
     keyboard.press(Key.up)
+    keyboard.release(Key.up)
     keyboard.press(Key.up)
+    keyboard.release(Key.up)
     keyboard.press(Key.up)
+    keyboard.release(Key.up)
     keyboard.press(Key.left)
+    keyboard.release(Key.left)
     keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 
     # now navigate putty
     keyboard.press(Key.tab)
+    keyboard.release(Key.tab)
     keyboard.press(Key.tab)
+    keyboard.release(Key.tab)
     keyboard.press(Key.right)
+    keyboard.release(Key.right)
     keyboard.press(Key.right)
+    keyboard.release(Key.right)
     # ssh should now be set
 
     keyboard.press(Key.tab)
+    keyboard.release(Key.tab)
     keyboard.press(Key.tab)
+    keyboard.release(Key.tab)
     keyboard.press(Key.tab)
+    keyboard.release(Key.tab)
     keyboard.press(Key.tab)
+    keyboard.release(Key.tab)
     keyboard.press(Key.tab)
+    keybard.release(key.tab)
 
     keyboard.press(Key.down)
+    keyboard.release(Key.dow)
     keyboard.press(Key.down)
+    keyboard.release(Key.down)
     keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 
     username = "alexandra"
     password = "hacking1972"
